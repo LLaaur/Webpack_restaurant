3ec063dfb956235138abe4da94a750a3cfb3991e
Laur <laur_laurentiu2013@yahoo.com>
2022-10-11 19:35:42 +0300

    create main header of the website

--- src/websiteLoad.js
+++ src/websiteLoad.js
@@ -1,18 +1,51 @@
 import loadHome from "./home";
 
-function initWebsite(){
-    const content = document.getElementById('content');
+function createHeader(){
+    const header = document.createElement('div');
+    header.classList.add('header');
 
-    content.appendChild(createMain());
+    header.appendChild(createNav());
 
-    loadHome();
+    return header;
 }
+
+function createNav(){
+    const nav = document.createElement('nav');
+    nav.classList.add('nav');
+
+    const homeBtn = document.createElement('button');
+    homeBtn.textContent = 'Acasa';
+    
+    const menuBtn = document.createElement('button');
+    menuBtn.textContent = 'Meniu';
+
+    const contactBtn = document.createElement('button');
+    contactBtn.textContent = 'Contact';
+
+    nav.appendChild(homeBtn);
+    nav.appendChild(menuBtn);
+    nav.appendChild(contactBtn);
+
+    return nav;
+}
+
 
 function createMain(){
     const main = document.createElement("main");
     main.classList.add("main");
     main.setAttribute("id", "main");
     return main;
+}
+
+
+function initWebsite(){
+    const content = document.getElementById('content');
+
+    content.appendChild(createHeader());
+
+    content.appendChild(createMain());
+
+    loadHome();
 }
 
 export default initWebsite;
